13a7947da773f83a1517743bff4ab0fb1aa0f23d
Gimmikku <sakuyastits@outlook.com>
2017-03-15 23:12:58 +0100

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,1 +1,7 @@
+require 'discordrb'
 
+discord_bot = Discordrb::Commands::CommandBot.new token: 'MjkxNjkyMTU5OTk4ODg1ODk5.C6tNcw.tzCoVhVDLHxVJlBPOvWFx9wfXi0', client_id: 291692159998885899
+
+end
+
+discord_but.run
