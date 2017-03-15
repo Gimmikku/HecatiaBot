3ed36e692c75ab795ec014b38814eabc05a802af
Cocoron <sakuyastits@outlook.com>
2017-03-15 23:29:07 +0000

    Added a command.

--- main.rb
+++ main.rb
@@ -1,7 +1,9 @@
 require 'discordrb'
 
-discord_bot = Discordrb::Commands::CommandBot.new token: 'MjkxNjkyMTU5OTk4ODg1ODk5.C6tNcw.tzCoVhVDLHxVJlBPOvWFx9wfXi0', client_id: 291692159998885899
+bot = Discordrb::Commands::CommandBot.new token: 'MjkxNjkyMTU5OTk4ODg1ODk5.C6tcuw.lLpfQMk4fs1zFg6qPMXERSNiiKo', client_id: 160123456789876543, prefix: '~'
 
+bot.command :hello do |event|
+  event << 'Welcome :heart: Hell'
 end
 
-discord_but.run
+bot.run
