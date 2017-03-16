25095d45b8510c959114a332f0fd465bd0e2646a
Cocoron <sakuyastits@outlook.com>
2017-03-16 00:22:51 +0000

    Added ~fortune

--- main.rb
+++ main.rb
@@ -6,4 +6,12 @@ bot.command :hello do |event|
   event << 'Welcome :heart: Hell'
 end
 
+fortune_verb = ["kill", "tackle", "smoosh", "hug", "clean", "purify", "love", "fight", "explode", "find", "pray", "sing", "grow", "hide", "bounce","read"]
+fortune_adj = ["sparkly","angel","devil","rainbow","crystal","brave","pretty","weird","ugly","fat","spooky","ancient","new","squishy","dirty","smart"] 
+fortune_noun = ["gold","knight","ghost","box","song","sword","ghost","love","imp","card","cummie","nerd","orb","potion","wizard","princess","power"]
+
+bot.command :fortune do |event|
+  event << "You will #{fortune_verb.sample} #{fortune_adj.sample} #{fortune_noun.sample}."
+end
+
 bot.run
